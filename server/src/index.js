6270264ae8e13b7a20fd86ec71ee87a7472c710e
adarsh3002 <ak47kumaradarsh@gmail.com>
2024-10-02 23:47:05 +0530

    added uptted cors

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,11 @@ const app = express();
 dotenv.config();
 
 
-app.use(cors())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 console.log("first")
 app.use(express.json());
 app.use(cookieParser());
